e39884796a9c95b4ac8a8fcbe5d88d88e3ff51db
artemdatsenko <dacenko.aa@yandex.ru>
2024-03-09 20:21:29 +0300

    отрисовали кнопки

--- static/script_1.js
+++ static/script_1.js
@@ -1,33 +1,51 @@
-let button = document.querySelector('.button');
-let button2 = document.querySelector('.button2');
+document.addEventListener('DOMContentLoaded', function (event) {
 
-let inner = document.querySelector('.inner');
-let form = document.querySelector('form')
-let duration = 500
 
-function animate() {
-    inner.style.height = '0'
-    setTimeout(function () {
-        document.querySelector('form').style.display = 'block'
-        inner.style.height = form.offsetHeight + 'px'
-    }, duration)
+    let inner = document.querySelector('.inner');
+    let form = document.querySelector('form')
+    let duration = 500
+    let full_url_element = document.getElementById('full-url');
+    let full_url = full_url_element.getAttribute('data-full-url');
 
-}
+    fetch(full_url)
+        .then(response => response.json())
+        .then(data => {
+            // отрисовали кнопки
+            for (fist_param in data) {
+                let button = document.createElement('button')
+                button.textContent = fist_param
+                let buttonsContainer = document.querySelector('.buttons');
+
+                buttonsContainer.appendChild(button);
+
+                console.log(document.querySelector('.buttons'))
+            }
+        })
+
+    function animate() {
+        inner.style.height = '0'
+        setTimeout(function () {
+            document.querySelector('form').style.display = 'block'
+            inner.style.height = form.offsetHeight + 'px'
+        }, duration)
+
+    }
 
 // Добавляем обработчик события на клик по кнопке
-button.addEventListener('click', function () {
+    button.addEventListener('click', function () {
 
-    animate()
-    setTimeout(function () {
-        // формы и поля
-    }, duration)
+        animate()
+        setTimeout(function () {
+            // формы и поля
+        }, duration)
 
-})
-button2.addEventListener('click', function () {
-    animate()
-    setTimeout(function () {
-        // формы и поля
-    }, duration)
+    })
+    button2.addEventListener('click', function () {
+        animate()
+        setTimeout(function () {
+            // формы и поля
+        }, duration)
 
 
+    })
 })
