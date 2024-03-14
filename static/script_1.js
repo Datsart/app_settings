b0a0591da4cddc5a03659db0d8ae2b8f0751ea59
artemdatsenko <dacenko.aa@yandex.ru>
2024-03-14 18:14:37 +0300

    вывод параметров при кликах выбор первого основного параметра и отрисовка его

--- static/script_1.js
+++ static/script_1.js
@@ -1,51 +1,68 @@
 document.addEventListener('DOMContentLoaded', function (event) {
 
-
     let inner = document.querySelector('.inner');
-    let form = document.querySelector('form')
-    let duration = 500
+    let form = document.querySelector('form');
+    let duration = 500;
     let full_url_element = document.getElementById('full-url');
     let full_url = full_url_element.getAttribute('data-full-url');
-
     fetch(full_url)
         .then(response => response.json())
         .then(data => {
             // отрисовали кнопки
-            for (fist_param in data) {
-                let button = document.createElement('button')
-                button.textContent = fist_param
+            for (let firstParam in data) {
+                console.log(firstParam)
+                let button = document.createElement('button');
+                button.textContent = firstParam;
+                button.classList.add('button_' + firstParam);
+                button.dataset.div = firstParam
                 let buttonsContainer = document.querySelector('.buttons');
-
                 buttonsContainer.appendChild(button);
 
-                console.log(document.querySelector('.buttons'))
+                // создаем класс ДИВОВ по первым основным параметрам
+                let div = document.createElement('div');
+                div.classList.add('field');
+                div.id = firstParam
+                let divContainer = document.querySelector('form');
+                let title = document.createElement('p')
+                title.textContent = firstParam
+                div.append(title)
+                // divContainer.appendChild(title)
+                divContainer.append(div);
+                for (let second_param in data[firstParam]) {
+                    console.log(second_param)
+                    console.log(data[firstParam][second_param]['text'])
+                    console.log(data[firstParam][second_param]['data_type'])
+                    console.log(data[firstParam][second_param]['value'])
+
+
+
+                }
+
             }
-        })
+            let buttons = document.querySelectorAll('.buttons button');
+            buttons.forEach(function (button) {
+                button.addEventListener('click', function (e) {
+                    animate();
+                    let button = e.target
+                    document.querySelectorAll('.buttons button').forEach(function (e) {
+                        let form_div = document.getElementById(e.dataset.div)
+                        form_div.classList.add('hide')
+                        form_div.classList.remove('show')
+
+                    })
+                    document.getElementById(button.dataset.div).classList.add('show')
+                });
+            })
+
+
+        });
 
     function animate() {
-        inner.style.height = '0'
+        inner.style.height = '0';
         setTimeout(function () {
-            document.querySelector('form').style.display = 'block'
-            inner.style.height = form.offsetHeight + 'px'
-        }, duration)
-
+            document.querySelector('form').style.display = 'block';
+            inner.style.height = form.offsetHeight + 'px';
+        }, duration);
     }
 
-// Добавляем обработчик события на клик по кнопке
-    button.addEventListener('click', function () {
-
-        animate()
-        setTimeout(function () {
-            // формы и поля
-        }, duration)
-
-    })
-    button2.addEventListener('click', function () {
-        animate()
-        setTimeout(function () {
-            // формы и поля
-        }, duration)
-
-
-    })
-})
+});
