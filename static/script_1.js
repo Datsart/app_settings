3d1fd5f31867d2df093f1152051482af2627413f
artemdatsenko <dacenko.aa@yandex.ru>
2024-03-14 18:49:11 +0300

    отрисовываются поля

--- static/script_1.js
+++ static/script_1.js
@@ -21,22 +21,43 @@ document.addEventListener('DOMContentLoaded', function (event) {
                 // создаем класс ДИВОВ по первым основным параметрам
                 let div = document.createElement('div');
                 div.classList.add('field');
-                div.id = firstParam
+                div.id = firstParam;
                 let divContainer = document.querySelector('form');
-                let title = document.createElement('p')
-                title.textContent = firstParam
-                div.append(title)
-                // divContainer.appendChild(title)
-                divContainer.append(div);
+                let title = document.createElement('p');
+                title.textContent = firstParam;
+                div.append(title);
+
                 for (let second_param in data[firstParam]) {
-                    console.log(second_param)
-                    console.log(data[firstParam][second_param]['text'])
-                    console.log(data[firstParam][second_param]['data_type'])
-                    console.log(data[firstParam][second_param]['value'])
+                    let label = document.createElement('label');
+                    label.textContent = data[firstParam][second_param]['text'];
 
+                    let input = document.createElement('input');
+                    input.setAttribute('type', data[firstParam][second_param]['data_type']);
+                    input.value = data[firstParam][second_param]['value'];
 
-
+                    div.append(label);
+                    div.append(input);
                 }
+
+                divContainer.append(div);
+
+                // let div = document.createElement('div');
+                // div.classList.add('field');
+                // div.id = firstParam
+                // let divContainer = document.querySelector('form');
+                // let title = document.createElement('p')
+                // title.textContent = firstParam
+                // div.append(title)
+                // divContainer.append(div);
+                // for (let second_param in data[firstParam]) {
+                //     console.log(second_param)
+                //     console.log(data[firstParam][second_param]['text'])
+                //     console.log(data[firstParam][second_param]['data_type'])
+                //     console.log(data[firstParam][second_param]['value'])
+                //
+                //
+                //
+                // }
 
             }
             let buttons = document.querySelectorAll('.buttons button');
